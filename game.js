fa30b3fc9c8d3b14a2749b4f943a5922afc820b7
EMarquez27 <105992805+EMarquez27@users.noreply.github.com>
2023-06-01 17:55:34 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,143 @@
+class Demo1 extends AdventureScene {
+    constructor() {
+        super("demo1", "First Room");
+    }
+
+    onEnter() {
+
+        let clip = this.add.text(this.w * 0.3, this.w * 0.3, "📎 paperclip")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => this.showMessage("Metal, bent."))
+            .on('pointerdown', () => {
+                this.showMessage("No touching!");
+                this.tweens.add({
+                    targets: clip,
+                    x: '+=' + this.s,
+                    repeat: 2,
+                    yoyo: true,
+                    ease: 'Sine.inOut',
+                    duration: 100
+                });
+            });
+
+        let key = this.add.text(this.w * 0.5, this.w * 0.1, "🔑 key")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => {n
+                this.showMessage("It's a nice key.")
+            })
+            .on('pointerdown', () => {
+                this.showMessage("You pick up the key.");
+                this.gainItem('key');
+                this.tweens.add({
+                    targets: key,
+                    y: `-=${2 * this.s}`,
+                    alpha: { from: 1, to: 0 },
+                    duration: 500,
+                    onComplete: () => key.destroy()
+                });
+            })
+
+        let door = this.add.text(this.w * 0.1, this.w * 0.15, "🚪 locked door")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => {
+                if (this.hasItem("key")) {
+                    this.showMessage("You've got the key for this door.");
+                } else {
+                    this.showMessage("It's locked. Can you find a key?");
+                }
+            })
+            .on('pointerdown', () => {
+                if (this.hasItem("key")) {
+                    this.loseItem("key");
+                    this.showMessage("*squeak*");
+                    door.setText("🚪 unlocked door");
+                    this.gotoScene('demo2');
+                }
+            })
+
+    }
+}
+
+class Demo2 extends AdventureScene {
+    constructor() {
+        super("demo2", "The second room has a long name (it truly does).");
+    }
+    onEnter() {
+        this.add.text(this.w * 0.3, this.w * 0.4, "just go back")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage("You've got no other choice, really.");
+            })
+            .on('pointerdown', () => {
+                this.gotoScene('demo1');
+            });
+
+        let finish = this.add.text(this.w * 0.6, this.w * 0.2, '(finish the game)')
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage('*giggles*');
+                this.tweens.add({
+                    targets: finish,
+                    x: this.s + (this.h - 2 * this.s) * Math.random(),
+                    y: this.s + (this.h - 2 * this.s) * Math.random(),
+                    ease: 'Sine.inOut',
+                    duration: 500
+                });
+            })
+            .on('pointerdown', () => this.gotoScene('outro'));
+    }
+}
+
+class Intro extends Phaser.Scene {
+    constructor() {
+        super('intro')
+    }
+    create() {
+        this.add.text(50,50, 
+            "It's the weekend again. I need to replenish the fridge. You can do it. Cmon just take a step outside, me. ").setFontSize(50);
+        this.add.text(50,100, "Click anywhere to begin.").setFontSize(20);
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('demo1'));
+        });
+    }
+}
+
+class GoodEnd extends Phaser.Scene {
+    constructor() {
+        super('goodend');
+    }
+    create() {
+        this.add.text(50, 50, "Phew! You reached your car without embarassing yourself. Time to head out.").setFontSize(50);
+        this.add.text(50, 100, "Click anywhere to restart.").setFontSize(20);
+        this.input.on('pointerdown', () => this.scene.start('intro'));
+    }
+}
+
+class BadEnd extends Phaser.Scene {
+    constructor() {
+        super('badend');
+    }
+    create() {
+        this.add.text(50, 50, "Large hands cover your eyes and mouth. Your heartbeat quickens and the panic makes you faint. I knew I shouldn't have gone outside.").setFontSize(50);
+        this.add.text(50, 100, "Click anywhere to restart.").setFontSize(20);
+        this.input.on('pointerdown', () => this.scene.start('intro'));
+    }
+}
+
+
+const game = new Phaser.Game({
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+    },
+    scene: [Intro, Demo1, Demo2, GoodEnd, BadEnd],
+    title: "Adventure Game",
+});
+
